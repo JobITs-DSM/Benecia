9aa3024d7698a2fa02ff3c324fd23752b75481f9
jhhong0509 <jhhong0509@naver.com>
2021-11-15 19:11:07 +0900

    [MODIFY] <WelfareCode> 중식 제공을 석식 제공으로 수정

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/code/WelfareCode.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/code/WelfareCode.java
@@ -9,7 +9,7 @@ public enum WelfareCode {
 
     DORMITORY("WEL001", "기숙사 제공"),
     MEAL("WEL002", "식사 제공"),
-    LAUNCH("WEL003", "중식 제공"),
+    LAUNCH("WEL003", "석식 제공"),
     SELF_DEVELOP_COST("WEL004", "자기계발비"),
     ARMY_EXCEPTION_REQUEST("WEL005", "병특 신청"),
     YOUTH_RECRUITMENT("WEL006", "청년내일채움공채");
