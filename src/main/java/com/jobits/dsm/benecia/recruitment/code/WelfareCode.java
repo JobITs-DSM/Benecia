92939af87d80fc635b4d8d1b89f44a921b2f711f
jhhong0509 <jhhong0509@naver.com>
2021-11-15 19:04:42 +0900

    [ADD] <WelfareCode.java> 복리후생 코드 추가

---
+++ src/main/java/com/jobits/dsm/benecia/recruitment/code/WelfareCode.java
@@ -0,0 +1,20 @@
+package com.jobits.dsm.benecia.recruitment.code;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+public enum WelfareCode {
+
+    DORMITORY("WEL001", "기숙사 제공"),
+    MEAL("WEL002", "식사 제공"),
+    LAUNCH("WEL003", "중식 제공"),
+    SELF_DEVELOP_COST("WEL004", "자기계발비"),
+    ARMY_EXCEPTION_REQUEST("WEL005", "병특 신청"),
+    YOUTH_RECRUITMENT("WEL006", "청년내일채움공채");
+
+    private final String code;
+    private final String value;
+
+}
