6e5861a36613716c6e166742d0331be037295dd6
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-22 19:13:55 +0900

    [ADD] <ProgrammingLanguageRepository>

---
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/programminglanguag/ProgrammingLanguageRepository.java
@@ -0,0 +1,8 @@
+package com.jobits.dsm.benecia.domain.recruitment.domain.programminglanguag;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.UUID;
+
+public interface ProgrammingLanguageRepository extends CrudRepository<ProgrammingLanguage, UUID> {
+}
