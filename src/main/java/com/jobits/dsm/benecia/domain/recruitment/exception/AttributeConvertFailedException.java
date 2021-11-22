3e811a0e39e35178a6b4cd876aad6216a2ac9d97
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-22 19:28:23 +0900

    [ADD] <AttributeConvertFailedException>

---
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/exception/AttributeConvertFailedException.java
@@ -0,0 +1,8 @@
+package com.jobits.dsm.benecia.domain.recruitment.exception;
+
+public class AttributeConvertFailedException extends RuntimeException {
+
+    public AttributeConvertFailedException() {
+        super("Attribute Convert Failed");
+    }
+}
