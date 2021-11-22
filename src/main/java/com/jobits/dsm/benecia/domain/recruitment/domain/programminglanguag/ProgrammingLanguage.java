2c50b2579855e881e0e14c7443813e8b94dc5514
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-11-22 19:13:40 +0900

    [ADD] <ProgrammingLanguage>

---
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/programminglanguag/ProgrammingLanguage.java
@@ -0,0 +1,20 @@
+package com.jobits.dsm.benecia.domain.recruitment.domain.programminglanguag;
+
+import com.jobits.dsm.benecia.domain.recruitment.code.ProgrammingLanguageCode;
+import lombok.*;
+
+import javax.persistence.*;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class ProgrammingLanguage {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Convert(converter = ProgrammingLanguageCode.ProgrammingLanguageCodeConverter.class)
+    private ProgrammingLanguageCode code;
+}
