a119f53f10ee5779540cdd2b3c69bede370b8968
jhhong0509 <jhhong0509@naver.com>
2021-12-01 21:40:25 +0900

    [ADD] #2 Recruitment 엔티티 추가

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/Recruitment.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/Recruitment.java
@@ -1,2 +1,21 @@
-package com.jobits.dsm.benecia.domain.recruitment.domain;public class Recruitment {
+package com.jobits.dsm.benecia.domain.recruitment.domain;
+
+import com.jobits.dsm.benecia.domain.recruitment.code.RecruitmentStatusCode;
+import lombok.*;
+
+import javax.persistence.EmbeddedId;
+import javax.persistence.Entity;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Entity
+public class Recruitment {
+
+    @EmbeddedId
+    private RecruitmentId recruitmentId;
+
+    private RecruitmentStatusCode status;
 }
