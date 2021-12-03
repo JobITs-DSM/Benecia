a946505c52a3649e17d50ea80a1bdd1429612841
parksangwoo <b01056721617@gmail.com>
2021-12-04 00:45:02 +0900

    [MODIFY] #86 컬럼명 카멜케이스로 변경

--- src/main/java/com/jobits/dsm/benecia/domain/contract/domain/Contract.java
+++ src/main/java/com/jobits/dsm/benecia/domain/contract/domain/Contract.java
@@ -3,6 +3,8 @@ package com.jobits.dsm.benecia.domain.contract.domain;
 import lombok.*;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -14,8 +16,9 @@ import java.time.LocalDateTime;
 @Getter
 public class Contract {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @NotNull
-    private LocalDateTime date_time;
+    private LocalDateTime dateTime;
 }
