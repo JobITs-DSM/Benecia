1939b27a57c1e56c35b3394348099096e24f6659
parksangwoo <b01056721617@gmail.com>
2021-12-04 00:21:14 +0900

    [MODIFY] #3 생성자, Builder, Getter 추가

--- src/main/java/com/jobits/dsm/benecia/domain/enterprise/domain/Branch.java
+++ src/main/java/com/jobits/dsm/benecia/domain/enterprise/domain/Branch.java
@@ -1,10 +1,16 @@
 package com.jobits.dsm.benecia.domain.enterprise.domain;
 
+import lombok.*;
+
 import javax.persistence.Embeddable;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
 public class Branch {
     @NotNull
     @Size(min = 5, max = 5)
