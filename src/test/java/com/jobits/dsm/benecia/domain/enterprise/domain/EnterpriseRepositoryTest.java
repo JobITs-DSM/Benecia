cb0b7db9fab8bbddbd664ead301f7711bde0a031
parksangwoo <b01056721617@gmail.com>
2021-12-04 00:20:42 +0900

    [MODIFY] #3 애그리거트 엔티티 테스트 추가

--- src/test/java/com/jobits/dsm/benecia/domain/enterprise/domain/EnterpriseRepositoryTest.java
+++ src/test/java/com/jobits/dsm/benecia/domain/enterprise/domain/EnterpriseRepositoryTest.java
@@ -1,5 +1,7 @@
 package com.jobits.dsm.benecia.domain.enterprise.domain;
 
+import com.jobits.dsm.benecia.domain.enterprise.code.EnterpriseDivisionCode;
+import com.jobits.dsm.benecia.domain.enterprise.code.EnterpriseEmployeeCountCode;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,15 +29,73 @@ class EnterpriseRepositoryTest {
         String tmp = "000-00-00000";
         Enterprise enterprise = Enterprise.builder()
                 .registration_number(tmp)
+                .name("잡플래닛")
+                .establish_year("2004")
+                .is_convention(true)
+                .division(EnterpriseDivisionCode.LEADING_COMPANY)
+                .representative_name("박상우")
+                .headquarter(Headquarter.builder()
+                        .postal_code("11111")
+                        .address("headquarter_address")
+                        .address_detail("headquarter_address_detail")
+                        .build()
+                )
+                .branch(Branch.builder()
+                        .branch_postal_code("11111")
+                        .branch_address("branch_address")
+                        .branch_address_detail("branch_address_detail")
+                        .build()
+                )
+                .introduction("회사 소개")
+                .employee_count(EnterpriseEmployeeCountCode.FIFTY_OR_MORE)
+                .site("url")
+                .turnover(128394)
+                .director(Director.builder()
+                        .director_email("director@gmail.com")
+                        .director_name("박상우")
+                        .director_telephone_number("01012345678")
+                        .director_phone_number("01023462123")
+                        .director_department("팀장")
+                        .build()
+                )
                 .build();
         assertThat(enterpriseRepository.save(enterprise).getRegistration_number()).isEqualTo(tmp);
     }
 
     @Test
     void 저장_테스트_실패_SIZE_초과() {
-        String tmp = "123456789101112";
+        String tmp = "000-0000-00000";
         Enterprise enterprise = Enterprise.builder()
                 .registration_number(tmp)
+                .name("잡플래닛")
+                .establish_year("2004")
+                .is_convention(true)
+                .division(EnterpriseDivisionCode.LEADING_COMPANY)
+                .representative_name("박상우")
+                .headquarter(Headquarter.builder()
+                        .postal_code("11111")
+                        .address("headquarter_address")
+                        .address_detail("headquarter_address_detail")
+                        .build()
+                )
+                .branch(Branch.builder()
+                        .branch_postal_code("11111")
+                        .branch_address("branch_address")
+                        .branch_address_detail("branch_address_detail")
+                        .build()
+                )
+                .introduction("회사 소개")
+                .employee_count(EnterpriseEmployeeCountCode.FIFTY_OR_MORE)
+                .site("url")
+                .turnover(128394)
+                .director(Director.builder()
+                        .director_email("director@gmail.com")
+                        .director_name("박상우")
+                        .director_telephone_number("01012345678")
+                        .director_phone_number("01023462123")
+                        .director_department("팀장")
+                        .build()
+                )
                 .build();
         assertThrows(ConstraintViolationException.class, () -> enterpriseRepository.saveAndFlush(enterprise));
     }
