8f14ad108f6b24832540c70d4ba12a7363d009f0
parksangwoo <b01056721617@gmail.com>
2021-12-04 00:22:11 +0900

    [MODIFY] #3 생성자, Builder, Getter 추가

--- src/main/java/com/jobits/dsm/benecia/domain/enterprise/domain/Director.java
+++ src/main/java/com/jobits/dsm/benecia/domain/enterprise/domain/Director.java
@@ -1,12 +1,20 @@
 package com.jobits.dsm.benecia.domain.enterprise.domain;
 
+import lombok.*;
+
 import javax.persistence.Embeddable;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Embeddable
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Builder
+@Getter
 public class Director {
     @NotNull
+    @Email
     @Size(max = 320)
     private String director_email;
 
