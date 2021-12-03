b73a1064b80a941b53649d7064dd5a7a15104dbf
parksangwoo <b01056721617@gmail.com>
2021-12-03 21:55:04 +0900

    [ADD] #88 TrainingRepository 추가

---
+++ src/main/java/com/jobits/dsm/benecia/domain/training/domain/TrainingRepository.java
@@ -0,0 +1,6 @@
+package com.jobits.dsm.benecia.domain.training.domain;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrainingRepository extends JpaRepository<Training, Integer> {
+}
