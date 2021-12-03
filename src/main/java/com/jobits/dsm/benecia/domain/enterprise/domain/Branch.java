caa582b82030bbf131a8c8c3c99cb2c7e368d583
parksangwoo <b01056721617@gmail.com>
2021-12-03 20:21:29 +0900

    [ADD] #3 지점 Embedded Type 추가

---
+++ src/main/java/com/jobits/dsm/benecia/domain/enterprise/domain/Branch.java
@@ -0,0 +1,20 @@
+package com.jobits.dsm.benecia.domain.enterprise.domain;
+
+import javax.persistence.Embeddable;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Embeddable
+public class Branch {
+    @NotNull
+    @Size(min = 5, max = 5)
+    private String branch_postal_code;
+
+    @NotNull
+    @Size(max = 255)
+    private String branch_address;
+
+    @NotNull
+    @Size(max = 255)
+    private String branch_address_detail;
+}
