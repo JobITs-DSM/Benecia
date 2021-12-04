7fb67a9cd1e99f98db86e9f35c61673ce847957b
jeongjiwoo0522 <jiwoourty@gmail.com>
2021-12-04 14:56:17 +0900

    [FIX] #3 테스트 코드 수정

--- src/test/java/com/jobits/dsm/benecia/domain/enterprise/domain/EnterpriseRepositoryTest.java
+++ src/test/java/com/jobits/dsm/benecia/domain/enterprise/domain/EnterpriseRepositoryTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
 
 import javax.validation.ConstraintViolationException;
 
@@ -27,76 +26,56 @@ class EnterpriseRepositoryTest {
     @Test
     void 저장_테스트_성공() {
         String tmp = "000-00-00000";
-        Enterprise enterprise = Enterprise.builder()
-                .registration_number(tmp)
-                .name("잡플래닛")
-                .establish_year("2004")
-                .is_convention(true)
-                .division(EnterpriseDivisionCode.LEADING_COMPANY)
-                .representative_name("박상우")
-                .headquarter(Headquarter.builder()
-                        .postal_code("11111")
-                        .address("headquarter_address")
-                        .address_detail("headquarter_address_detail")
-                        .build()
-                )
-                .branch(Branch.builder()
-                        .branch_postal_code("11111")
-                        .branch_address("branch_address")
-                        .branch_address_detail("branch_address_detail")
-                        .build()
-                )
-                .introduction("회사 소개")
-                .employee_count(EnterpriseEmployeeCountCode.FIFTY_OR_MORE)
-                .site("url")
-                .turnover(128394)
-                .director(Director.builder()
-                        .director_email("director@gmail.com")
-                        .director_name("박상우")
-                        .director_telephone_number("01012345678")
-                        .director_phone_number("01023462123")
-                        .director_department("팀장")
-                        .build()
-                )
-                .build();
-        assertThat(enterpriseRepository.save(enterprise).getRegistration_number()).isEqualTo(tmp);
+
+        Enterprise enterprise = getEnterprise(tmp);
+
+        assertThat(enterpriseRepository.save(enterprise).getRegistrationNumber()).isEqualTo(tmp);
     }
 
     @Test
     void 저장_테스트_실패_SIZE_초과() {
         String tmp = "000-0000-00000";
-        Enterprise enterprise = Enterprise.builder()
-                .registration_number(tmp)
+
+        Enterprise enterprise = getEnterprise(tmp);
+
+        assertThrows(ConstraintViolationException.class, () -> enterpriseRepository.saveAndFlush(enterprise));
+    }
+
+    private Enterprise getEnterprise(String registrationNumber) {
+        Address address = Address.builder()
+                .postalCode("11111")
+                .address("headquarter_address")
+                .address("headquarter_address_detail")
+                .build();
+
+        Address branchAddress = Address.builder()
+                .postalCode("11111")
+                .address("branch_address")
+                .addressDetail("branch_address_detail")
+                .build();
+
+        Director director = Director.builder()
+                .email("director@gmail.com")
+                .name("박상우")
+                .telephoneNumber("01012345678")
+                .phoneNumber("01023462123")
+                .department("팀장")
+                .build();
+
+        return Enterprise.builder()
+                .registrationNumber(registrationNumber)
                 .name("잡플래닛")
-                .establish_year("2004")
-                .is_convention(true)
+                .establishYear("2004")
+                .isConvention(true)
                 .division(EnterpriseDivisionCode.LEADING_COMPANY)
-                .representative_name("박상우")
-                .headquarter(Headquarter.builder()
-                        .postal_code("11111")
-                        .address("headquarter_address")
-                        .address_detail("headquarter_address_detail")
-                        .build()
-                )
-                .branch(Branch.builder()
-                        .branch_postal_code("11111")
-                        .branch_address("branch_address")
-                        .branch_address_detail("branch_address_detail")
-                        .build()
-                )
+                .representativeName("박상우")
+                .address(address)
+                .branchAddress(branchAddress)
                 .introduction("회사 소개")
-                .employee_count(EnterpriseEmployeeCountCode.FIFTY_OR_MORE)
+                .employeeCount(EnterpriseEmployeeCountCode.FIFTY_OR_MORE)
                 .site("url")
                 .turnover(128394)
-                .director(Director.builder()
-                        .director_email("director@gmail.com")
-                        .director_name("박상우")
-                        .director_telephone_number("01012345678")
-                        .director_phone_number("01023462123")
-                        .director_department("팀장")
-                        .build()
-                )
+                .director(director)
                 .build();
-        assertThrows(ConstraintViolationException.class, () -> enterpriseRepository.saveAndFlush(enterprise));
     }
 }
