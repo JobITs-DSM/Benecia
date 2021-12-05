16b4e47886769f945b210a6ecfa8e0efb22449a1
parksangwoo <b01056721617@gmail.com>
2021-12-05 22:15:49 +0900

    [ADD] #98 Screening Process Repository 추가

---
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/screeningprocess/ScreeningProcessRepository.java
@@ -0,0 +1,6 @@
+package com.jobits.dsm.benecia.domain.recruitment.domain.screeningprocess;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScreeningProcessRepository extends JpaRepository<ScreeningProcess, Integer> {
+}
