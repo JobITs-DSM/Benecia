1dc703c961c389fa900a36f6ed02be3e54462e74
parksangwoo <b01056721617@gmail.com>
2021-12-08 19:55:53 +0900

    [FIX] #94 application entity와 매핑

--- src/main/java/com/jobits/dsm/benecia/domain/student/domain/Student.java
+++ src/main/java/com/jobits/dsm/benecia/domain/student/domain/Student.java
@@ -3,10 +3,7 @@ package com.jobits.dsm.benecia.domain.student.domain;
 import com.jobits.dsm.benecia.domain.application.domain.Application;
 import lombok.*;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
+import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
@@ -39,6 +36,6 @@ public class Student {
     @Size(max = 60)
     private String password;
 
-    @OneToMany(mappedBy = "studentSerialNumber", cascade = CascadeType.REMOVE)
+    @OneToMany(mappedBy = "studentSerialNumber", orphanRemoval = true, fetch = FetchType.EAGER)
     private List<Application> applications;
 }
