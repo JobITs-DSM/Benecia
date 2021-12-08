c17ddcf7e55011bc75422ff4e323879cd0dcad26
parksangwoo <b01056721617@gmail.com>
2021-12-08 20:14:04 +0900

    [FIX] #94 enablejpaauditing 추가

--- src/main/java/com/jobits/dsm/benecia/domain/application/domain/Application.java
+++ src/main/java/com/jobits/dsm/benecia/domain/application/domain/Application.java
@@ -4,6 +4,8 @@ import com.jobits.dsm.benecia.domain.application.code.ApplicationCode;
 import com.jobits.dsm.benecia.domain.recruitment.domain.hiringarea.HiringArea;
 import com.jobits.dsm.benecia.domain.student.domain.Student;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
@@ -14,6 +16,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
 @Builder
+@EntityListeners(AuditingEntityListener.class)
 public class Application {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -23,6 +26,7 @@ public class Application {
     private ApplicationCode status;
 
     @NotNull
+    @CreatedDate
     private LocalDateTime dateTime;
 
     @ManyToOne(fetch = FetchType.LAZY)
