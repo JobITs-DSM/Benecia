1d2ff4b12a2ee648e2ab024337ce91b24dbb6cfe
parksangwoo <b01056721617@gmail.com>
2021-12-11 23:47:23 +0900

    [ADD] #8 RecruitmentCodeController

---
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/presentation/RecruitmentCodeController.java
@@ -0,0 +1,21 @@
+package com.jobits.dsm.benecia.domain.recruitment.presentation;
+
+import com.jobits.dsm.benecia.domain.recruitment.service.RecruitmentCodeService;
+import com.jobits.dsm.benecia.global.code.CodeResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/recruitment/codes")
+public class RecruitmentCodeController {
+
+    private final RecruitmentCodeService recruitmentCodeService;
+
+    @GetMapping("/programming-language")
+    public CodeResponse programmingLanguage() {
+        return recruitmentCodeService.getProgrammingLanguage();
+    }
+}
