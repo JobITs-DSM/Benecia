65cca1a3b797bb35a25aa9ada6fa1051197d3ada
jhhong0509 <jhhong0509@naver.com>
2021-12-23 20:01:15 +0900

    [MODIFY] #78 SecurityConfig 수정

--- src/main/java/com/jobits/dsm/benecia/global/security/SecurityConfig.java
+++ src/main/java/com/jobits/dsm/benecia/global/security/SecurityConfig.java
@@ -1,17 +1,43 @@
 package com.jobits.dsm.benecia.global.security;
 
+import com.jobits.dsm.benecia.global.security.jwt.JwtTokenProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
+@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final JwtTokenProvider jwtTokenProvider;
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.apply(new FilterConfig());
+        http
+                .cors().and()
+                .csrf().disable()
+
+                .authorizeRequests()
+                    .antMatchers(HttpMethod.POST, "/v1/student/auth").permitAll()
+                    .antMatchers(HttpMethod.POST, "/v1/admin/auth").permitAll()
+                    .antMatchers(HttpMethod.POST, "/v1/enterprise/auth").permitAll()
+                    .antMatchers(HttpMethod.PUT, "/v1/auth").permitAll()
+                .anyRequest().authenticated()
+
+                .and()
+                .apply(new FilterConfig(jwtTokenProvider));
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
