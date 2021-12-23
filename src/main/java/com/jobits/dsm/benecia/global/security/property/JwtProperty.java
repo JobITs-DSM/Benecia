673569d0b5c5a57149c0c07c087a0530d8c33c8f
jhhong0509 <jhhong0509@naver.com>
2021-12-23 19:59:07 +0900

    [ADD] #78 Property 추가

---
+++ src/main/java/com/jobits/dsm/benecia/global/security/property/JwtProperty.java
@@ -0,0 +1,22 @@
+package com.jobits.dsm.benecia.global.security.property;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@AllArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties(prefix = "auth.jwt")
+public class JwtProperty {
+    private final String secretKey;
+    private final JwtExpProperty exp;
+
+    @Getter
+    @AllArgsConstructor
+    public static class JwtExpProperty {
+        private final Long access;
+        private final Long refresh;
+    }
+}
