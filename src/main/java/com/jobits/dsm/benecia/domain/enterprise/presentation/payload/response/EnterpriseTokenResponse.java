d372fd7c48593e9a543b60364ab6f7d51fe8c8f2
jhhong0509 <jhhong0509@naver.com>
2021-12-24 14:24:01 +0900

    [ADD] #124 <TokenRequest/Response> 기업 로그인 payload 추가

---
+++ src/main/java/com/jobits/dsm/benecia/domain/enterprise/presentation/payload/response/EnterpriseTokenResponse.java
@@ -0,0 +1,12 @@
+package com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response;
+
+import com.jobits.dsm.benecia.global.security.dto.Tokens;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.experimental.SuperBuilder;
+
+@Getter
+@SuperBuilder
+public class EnterpriseTokenResponse extends Tokens {
+
+}
