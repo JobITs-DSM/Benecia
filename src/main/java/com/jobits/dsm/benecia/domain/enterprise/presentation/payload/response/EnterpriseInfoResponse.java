f3dd79205835bd6076a33bd16c2240a1a83c5148
parksangwoo <b01056721617@gmail.com>
2021-12-30 20:23:53 +0900

    [ADD] #134 ResponseDTO

---
+++ src/main/java/com/jobits/dsm/benecia/domain/enterprise/presentation/payload/response/EnterpriseInfoResponse.java
@@ -0,0 +1,42 @@
+package com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response;
+import com.jobits.dsm.benecia.domain.enterprise.domain.Address;
+import com.jobits.dsm.benecia.domain.enterprise.domain.Director;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Getter
+@Builder
+public class EnterpriseInfoResponse {
+    private final String registrationNumber;
+    private final String name;
+    private final String establishYear;
+    private final String representativeName;
+    private final Address address;
+    private final Address branch;
+    private final String introduction;
+    private final String employeeCount;
+    private final String site;
+    private final Integer turnover;
+    private final Director director;
+    private final List<String> businessAreas;
+    private final AttachmentDetails businessLicense;
+    private final AttachmentDetails logo;
+    private final AttachmentDetails material;
+    private final AttachmentDetails foreground;
+
+    @Getter
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class AttachmentDetails {
+        private String fileName;
+        private String originalFileName;
+    }
+
+    public static AttachmentDetails of(String fileName, String originalFileName) {
+        return new AttachmentDetails(fileName, originalFileName);
+    }
+}
