c529d215de0b8fc7721ef7480b34ae1e6e2d82b1
parksangwoo <b01056721617@gmail.com>
2021-12-30 20:23:42 +0900

    [MODIFY] #134 기업 정보 확인 메소드 추가

--- src/main/java/com/jobits/dsm/benecia/domain/enterprise/service/EnterpriseService.java
+++ src/main/java/com/jobits/dsm/benecia/domain/enterprise/service/EnterpriseService.java
@@ -1,7 +1,7 @@
 package com.jobits.dsm.benecia.domain.enterprise.service;
 
-import com.jobits.dsm.benecia.domain.attatchment.domain.Attachment;
-import com.jobits.dsm.benecia.domain.attatchment.domain.AttachmentRepository;
+import com.jobits.dsm.benecia.domain.attachment.domain.Attachment;
+import com.jobits.dsm.benecia.domain.attachment.domain.AttachmentRepository;
 import com.jobits.dsm.benecia.domain.enterprise.code.BusinessAreaCode;
 import com.jobits.dsm.benecia.domain.enterprise.code.EnterpriseDivisionCode;
 import com.jobits.dsm.benecia.domain.enterprise.domain.Enterprise;
@@ -13,7 +13,9 @@ import com.jobits.dsm.benecia.domain.enterprise.domain.cache.EnterpriseRefreshTo
 import com.jobits.dsm.benecia.domain.enterprise.exceptions.EnterpriseNotFoundException;
 import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.request.EnterpriseSignInRequest;
 import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.request.RegisterEnterpriseRequest;
-import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response.EnterpriseListResponse;
+import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response.EnterpriseInfoListResponse;
+import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response.EnterpriseInfoResponse;
+import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response.EnterpriseInfoResponse.AttachmentDetails;
 import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response.EnterpriseTokenResponse;
 import com.jobits.dsm.benecia.global.security.dto.Tokens;
 import com.jobits.dsm.benecia.global.security.jwt.JwtTokenProvider;
@@ -96,10 +98,10 @@ public class EnterpriseService {
                 .build();
     }
 
-    public EnterpriseListResponse getEnterpriseList() {
-        return EnterpriseListResponse.builder()
+    public EnterpriseInfoListResponse getEnterpriseInfoList() {
+        return EnterpriseInfoListResponse.builder()
                 .enterprises(enterpriseRepository.findAll()
-                        .stream().map(enterprise -> EnterpriseListResponse.of(
+                        .stream().map(enterprise -> EnterpriseInfoListResponse.of(
                                 enterprise.getRegistrationNumber(),
                                 enterprise.getName(),
                                 enterprise.getAddress().getPostalCode(),
@@ -115,6 +117,38 @@ public class EnterpriseService {
                                 enterpriseRepository.getReviewCount(enterprise.getRegistrationNumber())
                         )).collect(Collectors.toList()))
                 .build();
+    }
+
+    public List<EnterpriseInfoResponse> getEnterpriseInfo(String request) {
+        return enterpriseRepository.findById(request)
+                .stream().map(enterprise -> EnterpriseInfoResponse.builder()
+                        .registrationNumber(enterprise.getRegistrationNumber())
+                        .name(enterprise.getName())
+                        .establishYear(enterprise.getEstablishYear())
+                        .representativeName(enterprise.getRepresentativeName())
+                        .address(enterprise.getAddress())
+                        .branch(enterprise.getBranchAddress())
+                        .introduction(enterprise.getIntroduction())
+                        .employeeCount(enterprise.getEmployeeCount().getValue())
+                        .site(enterprise.getSite())
+                        .turnover(enterprise.getTurnover())
+                        .director(enterprise.getDirector())
+                        .businessAreas(enterprise.getBusinessAreas()
+                                .stream().map(code -> code.getCode().getValue())
+                                .collect(Collectors.toList()))
+                        .businessLicense(checkNull(enterprise.getBusinessLicense()))
+                        .logo(checkNull(enterprise.getLogo()))
+                        .material(checkNull(enterprise.getMaterial()))
+                        .foreground(checkNull(enterprise.getForeground()))
+                        .build()
+                ).collect(Collectors.toList());
+    }
+
+    private AttachmentDetails checkNull(Attachment attachment) {
+        if (attachment == null) {
+            return null;
+        }
+        return EnterpriseInfoResponse.of(attachment.getFileName(), attachment.getOriginalFileName());
     }
 
     private void saveEnterpriseAttachment(Enterprise enterprise, MultipartFile file, Consumer<Attachment> consumer) {
