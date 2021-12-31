4e757491661215e400289a3bf33c43d3d9cefa12
parksangwoo <b01056721617@gmail.com>
2022-01-01 01:57:19 +0900

    [MODIFY] #138 기업 정보 update, delete 메소드 구현

--- src/main/java/com/jobits/dsm/benecia/domain/enterprise/domain/EnterpriseRepositoryCustom.java
+++ src/main/java/com/jobits/dsm/benecia/domain/enterprise/domain/EnterpriseRepositoryCustom.java
@@ -1,6 +1,8 @@
 package com.jobits.dsm.benecia.domain.enterprise.domain;
 
 import com.jobits.dsm.benecia.domain.enterprise.code.BusinessAreaCode;
+import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.request.ModifyEnterpriseInfoRequest;
+import org.springframework.data.jpa.repository.Modifying;
 
 import java.util.List;
 
@@ -10,4 +12,6 @@ public interface EnterpriseRepositoryCustom {
     Long getContractStudentCount(String registrationNumber);
 
     Long getReviewCount(String registrationNumber);
+
+    void modifyEnterpriseInfo(String registrationNumber, ModifyEnterpriseInfoRequest request);
 }
