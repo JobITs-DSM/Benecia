957c7f3a01e6559cf19cf806d064dae70daeab78
jeongjiwoo0522 <jiwoourty@gmail.com>
2022-01-09 22:07:35 +0900

    [FIX] #123 EnterpriseService 변경사항 맞춤

--- src/main/java/com/jobits/dsm/benecia/domain/enterprise/service/EnterpriseService.java
+++ src/main/java/com/jobits/dsm/benecia/domain/enterprise/service/EnterpriseService.java
@@ -1,6 +1,7 @@
 package com.jobits.dsm.benecia.domain.enterprise.service;
 
-import com.jobits.dsm.benecia.domain.attachment.facade.AttachmentFacade;
+import com.jobits.dsm.benecia.domain.attachment.domain.Attachment;
+import com.jobits.dsm.benecia.domain.attachment.domain.AttachmentRepository;
 import com.jobits.dsm.benecia.domain.enterprise.code.BusinessAreaCode;
 import com.jobits.dsm.benecia.domain.enterprise.code.EnterpriseDivisionCode;
 import com.jobits.dsm.benecia.domain.enterprise.domain.Enterprise;
@@ -11,30 +12,39 @@ import com.jobits.dsm.benecia.domain.enterprise.domain.cache.EnterpriseRefreshTo
 import com.jobits.dsm.benecia.domain.enterprise.domain.cache.EnterpriseRefreshTokenRepository;
 import com.jobits.dsm.benecia.domain.enterprise.exceptions.EnterpriseNotFoundException;
 import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.request.EnterpriseSignInRequest;
+import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.request.ModifyEnterpriseInfoRequest;
 import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.request.RegisterEnterpriseRequest;
 import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response.EnterpriseInfoListResponse;
+import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response.EnterpriseInfoResponse;
+import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response.EnterpriseInfoResponse.AttachmentDetails;
 import com.jobits.dsm.benecia.domain.enterprise.presentation.payload.response.EnterpriseTokenResponse;
 import com.jobits.dsm.benecia.global.security.dto.Tokens;
 import com.jobits.dsm.benecia.global.security.jwt.JwtTokenProvider;
 import com.jobits.dsm.benecia.global.security.property.JwtProperty;
 import com.jobits.dsm.benecia.global.security.property.JwtRoleProperty;
+import com.jobits.dsm.benecia.infrastructure.s3.S3Util;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
 public class EnterpriseService {
 
+    private final S3Util s3Util;
+
     private final EnterpriseRepository enterpriseRepository;
     private final EnterpriseRefreshTokenRepository refreshTokenRepository;
     private final BusinessAreaRepository businessAreaRepository;
+    private final AttachmentRepository attachmentRepository;
     private final JwtTokenProvider jwtTokenProvider;
     private final JwtProperty jwtProperty;
-
-    private final AttachmentFacade attachmentFacade;
 
     @Transactional
     public void registerEnterprise(RegisterEnterpriseRequest request) {
@@ -54,17 +64,18 @@ public class EnterpriseService {
                 .director(request.getDirector())
                 .build());
 
-        request.getBusinessAreas()
-                .forEach(businessAreaCode -> enterprise.getBusinessAreas()
-                        .add(businessAreaRepository.save(BusinessArea.builder()
-                                .code(businessAreaCode)
-                                .enterprise(enterprise)
-                                .build())));
+        saveEnterpriseAttachment(enterprise, request.getBusinessLicense(), enterprise::setBusinessLicense);
+        saveEnterpriseAttachment(enterprise, request.getLogo(), enterprise::setLogo);
+        saveEnterpriseAttachment(enterprise, request.getMaterial(), enterprise::setMaterial);
+        saveEnterpriseAttachment(enterprise, request.getForeground(), enterprise::setForeground);
 
-        attachmentFacade.saveAttachment(enterprise, request.getBusinessLicense(), enterprise::setBusinessLicense);
-        attachmentFacade.saveAttachment(enterprise, request.getLogo(), enterprise::setLogo);
-        attachmentFacade.saveAttachment(enterprise, request.getMaterial(), enterprise::setMaterial);
-        attachmentFacade.saveAttachment(enterprise, request.getForeground(), enterprise::setForeground);
+        request.getBusinessAreas()
+                .forEach(businessAreaCode -> {
+                    enterprise.getBusinessAreas().add(businessAreaRepository.save(BusinessArea.builder()
+                            .code(businessAreaCode)
+                            .enterprise(enterprise)
+                            .build()));
+                });
     }
 
     @Transactional
@@ -107,5 +118,77 @@ public class EnterpriseService {
                                 enterpriseRepository.getReviewCount(enterprise.getRegistrationNumber())
                         )).collect(Collectors.toList()))
                 .build();
+    }
+
+    public List<EnterpriseInfoResponse> getEnterpriseInfo(String request) {
+        return enterpriseRepository.findById(request)
+                .stream().map(enterprise -> EnterpriseInfoResponse.builder()
+                        .registrationNumber(enterprise.getRegistrationNumber())
+                        .name(enterprise.getName())
+                        .establishYear(enterprise.getEstablishYear())
+                        .representativeName(enterprise.getRepresentativeName())
+                        .address(enterprise.getAddress())
+                        .branch(enterprise.getBranchAddress())
+                        .introduction(enterprise.getIntroduction())
+                        .employeeCount(enterprise.getEmployeeCount().getValue())
+                        .site(enterprise.getSite())
+                        .turnover(enterprise.getTurnover())
+                        .director(enterprise.getDirector())
+                        .businessAreas(enterprise.getBusinessAreas()
+                                .stream().map(code -> code.getCode().getValue())
+                                .collect(Collectors.toList()))
+                        .businessLicense(checkNull(enterprise.getBusinessLicense()))
+                        .logo(checkNull(enterprise.getLogo()))
+                        .material(checkNull(enterprise.getMaterial()))
+                        .foreground(checkNull(enterprise.getForeground()))
+                        .build()
+                ).collect(Collectors.toList());
+    }
+
+    private AttachmentDetails checkNull(Attachment attachment) {
+        if (attachment == null) {
+            return null;
+        }
+        return new AttachmentDetails(attachment.getFileName(), attachment.getOriginalFileName());
+    }
+
+    @Transactional
+    public void modifyEnterpriseInfo(String registrationNumber, ModifyEnterpriseInfoRequest request) {
+        Enterprise enterprise = enterpriseRepository.findById(registrationNumber)
+                .orElseThrow(() -> EnterpriseNotFoundException.EXCEPTION);
+
+        enterpriseRepository.modifyEnterpriseInfo(registrationNumber, request);
+
+        enterprise.getBusinessAreas().clear();
+
+        request.getBusinessAreas()
+                .forEach(businessAreaCode -> {
+                    enterprise.getBusinessAreas().add(businessAreaRepository.save(BusinessArea.builder()
+                            .code(businessAreaCode)
+                            .enterprise(enterprise)
+                            .build()));
+                });
+    }
+
+    private void saveEnterpriseAttachment(Enterprise enterprise, MultipartFile file, Consumer<Attachment> consumer) {
+        Optional<String> savedFile = saveFileToStorage(file, "enterprise" + "/" + enterprise.getRegistrationNumber());
+        if (savedFile.isPresent()) {
+            Attachment attachment = saveFileToDatabase(savedFile.get(), file.getOriginalFilename());
+            consumer.accept(attachment);
+        }
+    }
+
+    private Optional<String> saveFileToStorage(MultipartFile file, String directoryName) {
+        if (file == null || file.isEmpty() || file.getOriginalFilename() == null) {
+            return Optional.empty();
+        }
+        return Optional.of(s3Util.saveFile(file, directoryName));
+    }
+
+    private Attachment saveFileToDatabase(String fileName, String originalFileName) {
+        return attachmentRepository.save(Attachment.builder()
+                .fileName(fileName)
+                .originalFileName(originalFileName)
+                .build());
     }
 }
