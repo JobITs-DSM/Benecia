e50b1d56aa5d077be85ae73e5fe64f131194ce65
parksangwoo <b01056721617@gmail.com>
2022-01-15 12:08:08 +0900

    [ADD] #126 RecruitmentRepositoryCustomImpl 추가

---
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepositoryCustomImpl.java
@@ -0,0 +1,34 @@
+package com.jobits.dsm.benecia.domain.recruitment.domain;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import static com.jobits.dsm.benecia.domain.enterprise.domain.QEnterprise.enterprise;
+import static com.jobits.dsm.benecia.domain.recruitment.domain.QRecruitment.recruitment;
+
+@RequiredArgsConstructor
+public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<RecruitmentVO> getRecruitmentInfoList() {
+        return queryFactory
+                .select(new QRecruitmentInfoVO(
+                                recruitment.recruitmentId.receptionYear,
+                                recruitment.registrationNumber,
+                                recruitment.status,
+                                enterprise.name,
+                                enterprise.division,
+                                recruitment.recruitmentDate.recruitBeginDate,
+                                recruitment.recruitmentDate.recruitEndDate
+                        )
+                )
+                .from(recruitment)
+                .join(recruitment.registrationNumber, enterprise)
+                .orderBy(recruitment.recruitmentDate.requestBeginDate.desc())
+                .fetch();
+    }
+}
