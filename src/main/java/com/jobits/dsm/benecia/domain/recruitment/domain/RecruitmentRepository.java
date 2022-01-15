21cf654c34aed978bb0d80f7426c5512fe3f9398
parksangwoo <b01056721617@gmail.com>
2022-01-15 12:07:33 +0900

    [MODIFY] #126 extends RecruitmentRepositoryCustom

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepository.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepository.java
@@ -4,5 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface RecruitmentRepository extends JpaRepository<Recruitment, RecruitmentId> {
+public interface RecruitmentRepository extends JpaRepository<Recruitment, RecruitmentId>, RecruitmentRepositoryCustom {
 }
