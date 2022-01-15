859c3a0491fdccfa32017795835c64eaccacbc29
parksangwoo <b01056721617@gmail.com>
2022-01-15 12:02:18 +0900

    [MODIFY] #126 extends HiringAreaRepositoryCustom

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/hiringarea/HiringAreaRepository.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/hiringarea/HiringAreaRepository.java
@@ -2,5 +2,5 @@ package com.jobits.dsm.benecia.domain.recruitment.domain.hiringarea;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface HiringAreaRepository extends JpaRepository<HiringArea, Integer> {
+public interface HiringAreaRepository extends JpaRepository<HiringArea, Integer>, HiringAreaRepositoryCustom {
 }
