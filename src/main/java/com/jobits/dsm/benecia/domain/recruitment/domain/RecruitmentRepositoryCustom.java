c7e47a4c005c59eeff25ff65f4080f7b98ef5251
parksangwoo <b01056721617@gmail.com>
2022-01-15 12:07:54 +0900

    [ADD] #126 RecruitmentRepositoryCustom 추가

---
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepositoryCustom.java
@@ -0,0 +1,7 @@
+package com.jobits.dsm.benecia.domain.recruitment.domain;
+
+import java.util.List;
+
+public interface RecruitmentRepositoryCustom {
+    List<RecruitmentVO> getRecruitmentInfoList();
+}
