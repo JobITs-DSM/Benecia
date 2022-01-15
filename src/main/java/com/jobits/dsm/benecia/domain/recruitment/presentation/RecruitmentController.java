8efbf1feb182eb7b34118762252066611ad54dcc
parksangwoo <b01056721617@gmail.com>
2022-01-15 12:09:12 +0900

    [ADD] #126 RecruitmentController 추가

---
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/presentation/RecruitmentController.java
@@ -0,0 +1,21 @@
+package com.jobits.dsm.benecia.domain.recruitment.presentation;
+
+import com.jobits.dsm.benecia.domain.recruitment.presentation.payload.response.RecruitmentInfoListResponse;
+import com.jobits.dsm.benecia.domain.recruitment.service.RecruitmentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/recruitment")
+public class RecruitmentController {
+
+    private final RecruitmentService recruitmentService;
+
+    @GetMapping
+    public RecruitmentInfoListResponse getRecruitmentInfoList() {
+        return recruitmentService.getRecruitmentInfoList();
+    }
+}
