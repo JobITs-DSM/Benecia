53decbadf2453ff24b77a7f9d7f60f787f1ee426
parksangwoo <b01056721617@gmail.com>
2022-04-08 17:13:04 +0900

    ⚡️ :: 쿼리에 맞게 VO 변경

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentVO.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentVO.java
@@ -1,31 +1,34 @@
 package com.jobits.dsm.benecia.domain.recruitment.domain;
 
 import com.jobits.dsm.benecia.domain.enterprise.code.EnterpriseDivisionCode;
-import com.jobits.dsm.benecia.domain.enterprise.domain.Enterprise;
+import com.jobits.dsm.benecia.domain.recruitment.code.HiringAreaCode;
 import com.jobits.dsm.benecia.domain.recruitment.code.RecruitmentStatusCode;
 import com.querydsl.core.annotations.QueryProjection;
 import lombok.Builder;
 import lombok.Getter;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Getter
 @Builder
 public class RecruitmentVO {
-    private final String receptionYear;
-    private final Enterprise registrationNumber;
     private final RecruitmentStatusCode status;
     private final String name;
+    private final List<HiringAreaCode> hiring;
+    private final Integer recruitmentCount;
+    private final Long applicantCount;
     private final EnterpriseDivisionCode division;
     private final LocalDate recruitBeginDate;
     private final LocalDate recruitEndDate;
 
     @QueryProjection
-    public RecruitmentVO(String receptionYear, Enterprise registrationNumber, RecruitmentStatusCode status, String name, EnterpriseDivisionCode division, LocalDate recruitBeginDate, LocalDate recruitEndDate) {
-        this.receptionYear = receptionYear;
-        this.registrationNumber = registrationNumber;
+    public RecruitmentVO(RecruitmentStatusCode status, String name, List<HiringAreaCode> hiring, Integer recruitmentCount, Long applicantCount, EnterpriseDivisionCode division, LocalDate recruitBeginDate, LocalDate recruitEndDate) {
         this.status = status;
         this.name = name;
+        this.hiring = hiring;
+        this.recruitmentCount = recruitmentCount;
+        this.applicantCount = applicantCount;
         this.division = division;
         this.recruitBeginDate = recruitBeginDate;
         this.recruitEndDate = recruitEndDate;
