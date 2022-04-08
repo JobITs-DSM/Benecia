01c14044957b22ea9a37181d3bccb499b8965008
parksangwoo <b01056721617@gmail.com>
2022-04-08 17:00:17 +0900

    ⚡️ :: Custom Repository 삭제

--- src/main/java/com/jobits/dsm/benecia/domain/application/domain/ApplicationRepository.java
+++ src/main/java/com/jobits/dsm/benecia/domain/application/domain/ApplicationRepository.java
@@ -2,5 +2,5 @@ package com.jobits.dsm.benecia.domain.application.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ApplicationRepository extends JpaRepository<Application, Integer>, ApplicationRepositoryCustom {
+public interface ApplicationRepository extends JpaRepository<Application, Integer>{
 }
