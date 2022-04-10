f6b9e2cd8640038a99d8d35ecfe92a82389238f5
parksangwoo <b01056721617@gmail.com>
2022-04-10 22:11:02 +0900

    ⚡️ :: (#126) 파라미터 추가

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/service/RecruitmentService.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/service/RecruitmentService.java
@@ -1,11 +1,15 @@
 package com.jobits.dsm.benecia.domain.recruitment.service;
 
 import com.jobits.dsm.benecia.domain.recruitment.code.HiringAreaCode;
+import com.jobits.dsm.benecia.domain.recruitment.code.RecruitmentStatusCode;
 import com.jobits.dsm.benecia.domain.recruitment.domain.RecruitmentRepository;
+import com.jobits.dsm.benecia.domain.recruitment.presentation.payload.request.RecruitmentInfoListRequest;
 import com.jobits.dsm.benecia.domain.recruitment.presentation.payload.response.RecruitmentInfoListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.stream.Collectors;
 
 @Service
@@ -14,9 +18,12 @@ public class RecruitmentService {
 
     private final RecruitmentRepository recruitmentRepository;
 
-    public RecruitmentInfoListResponse getRecruitmentInfoList() {
+    public RecruitmentInfoListResponse getRecruitmentInfoList(RecruitmentInfoListRequest request) {
+        RecruitmentStatusCode recruitmentStatus = RecruitmentStatusCode.find(request.getRecruitStatus());
+        LocalDate beginDate = LocalDate.parse(request.getBeginDate(), DateTimeFormatter.ISO_DATE);
+        LocalDate endDate = LocalDate.parse(request.getEndDate(), DateTimeFormatter.ISO_DATE);
         return RecruitmentInfoListResponse.builder()
-                .recruitments(recruitmentRepository.getRecruitmentInfoList() // null 처리 추가 요망
+                .recruitments(recruitmentRepository.getRecruitmentInfoList(request.getRecruitmentYear(), request.getKeyword(), recruitmentStatus, beginDate, endDate) // null 처리 추가 요망
                         .stream().map(recruitment -> RecruitmentInfoListResponse.RecruitmentInfo.builder()
                                 .status(recruitment.getStatus())
                                 .name(recruitment.getName())
