cc5b812c3fa07a50a846b12dd57bb332dfc477e0
parksangwoo <b01056721617@gmail.com>
2022-04-10 16:39:10 +0900

    ⚡️ :: 한 방 쿼리!!

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepositoryCustomImpl.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepositoryCustomImpl.java
@@ -1,12 +1,19 @@
 package com.jobits.dsm.benecia.domain.recruitment.domain;
 
+import com.querydsl.core.group.GroupBy;
+import com.querydsl.core.types.ExpressionUtils;
+import com.querydsl.core.types.dsl.Expressions;
+import com.querydsl.jpa.JPAExpressions;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
 import java.util.List;
 
-import static com.jobits.dsm.benecia.domain.enterprise.domain.QEnterprise.enterprise;
-import static com.jobits.dsm.benecia.domain.recruitment.domain.QRecruitment.recruitment;
+import static com.jobits.dsm.benecia.domain.application.domain.QApplication.*;
+import static com.jobits.dsm.benecia.domain.enterprise.domain.QEnterprise.*;
+import static com.jobits.dsm.benecia.domain.recruitment.domain.QRecruitment.*;
+import static com.jobits.dsm.benecia.domain.recruitment.domain.hiringarea.QHiringArea.*;
+import static com.querydsl.core.group.GroupBy.groupBy;
 
 @RequiredArgsConstructor
 public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCustom {
@@ -16,19 +23,30 @@ public class RecruitmentRepositoryCustomImpl implements RecruitmentRepositoryCus
     @Override
     public List<RecruitmentVO> getRecruitmentInfoList() {
         return queryFactory
-                .select(new QRecruitmentVO(
-                                recruitment.recruitmentId.receptionYear,
-                                recruitment.registrationNumber,
+                .selectFrom(recruitment)
+                .join(recruitment.hiringAreas, hiringArea)
+                .leftJoin(recruitment.applications, application)
+                .join(recruitment.enterprise, enterprise)
+                .orderBy(recruitment.recruitmentDate.recruitBeginDate.desc())
+                .transform(groupBy(recruitment.enterprise.registrationNumber, recruitment.recruitmentId.receptionYear, recruitment.recruitmentDate.recruitBeginDate)
+                        .list(new QRecruitmentVO(
                                 recruitment.status,
-                                enterprise.name,
+                                recruitment.enterprise.name,
+                                GroupBy.list(hiringArea.code),
+                                Expressions.asNumber(
+                                        JPAExpressions.select(hiringArea.count.sum())
+                                                .from(hiringArea)
+                                                .where(hiringArea.recruitment.recruitmentId.registrationNumber.eq(recruitment.recruitmentId.registrationNumber))
+                                ).intValue(),
+                                Expressions.asNumber(
+                                        JPAExpressions.select(application.count())
+                                                .from(application)
+                                                .where(application.recruitment.recruitmentId.registrationNumber.eq(recruitment.recruitmentId.registrationNumber))
+                                ).longValue(),
                                 enterprise.division,
                                 recruitment.recruitmentDate.recruitBeginDate,
                                 recruitment.recruitmentDate.recruitEndDate
-                        )
-                )
-                .from(recruitment)
-                .join(recruitment.registrationNumber, enterprise)
-                .orderBy(recruitment.recruitmentDate.requestBeginDate.desc())
-                .fetch();
+                        ))
+                );
     }
 }
