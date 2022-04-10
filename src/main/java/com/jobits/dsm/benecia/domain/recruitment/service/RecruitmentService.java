01dafcc6cee11984664c5ad92eaf9282316229fa
parksangwoo <b01056721617@gmail.com>
2022-04-10 16:38:15 +0900

    ⚡️ :: recruitmentCount, applicantCount 추가

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/service/RecruitmentService.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/service/RecruitmentService.java
@@ -1,14 +1,11 @@
 package com.jobits.dsm.benecia.domain.recruitment.service;
 
-import com.jobits.dsm.benecia.domain.application.domain.ApplicationRepository;
-import com.jobits.dsm.benecia.domain.enterprise.domain.Enterprise;
+import com.jobits.dsm.benecia.domain.recruitment.code.HiringAreaCode;
 import com.jobits.dsm.benecia.domain.recruitment.domain.RecruitmentRepository;
-import com.jobits.dsm.benecia.domain.recruitment.domain.hiringarea.HiringAreaRepository;
 import com.jobits.dsm.benecia.domain.recruitment.presentation.payload.response.RecruitmentInfoListResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.stream.Collectors;
 
 @Service
@@ -19,11 +16,13 @@ public class RecruitmentService {
 
     public RecruitmentInfoListResponse getRecruitmentInfoList() {
         return RecruitmentInfoListResponse.builder()
-                .recruitments(recruitmentRepository.getRecruitmentInfoList()
+                .recruitments(recruitmentRepository.getRecruitmentInfoList() // null 처리 추가 요망
                         .stream().map(recruitment -> RecruitmentInfoListResponse.RecruitmentInfo.builder()
                                 .status(recruitment.getStatus())
                                 .name(recruitment.getName())
-                                .hiring(getHiringInfoList(recruitment.getReceptionYear(), recruitment.getRegistrationNumber()))
+                                .hiring(recruitment.getHiring().stream().map(HiringAreaCode::getValue).collect(Collectors.toList()))
+                                .recruitCount(recruitment.getRecruitmentCount())
+                                .applicantCount(recruitment.getApplicantCount())
                                 .division(recruitment.getDivision())
                                 .recruitBeginDate(recruitment.getRecruitBeginDate())
                                 .recruitEndDate(recruitment.getRecruitEndDate())
