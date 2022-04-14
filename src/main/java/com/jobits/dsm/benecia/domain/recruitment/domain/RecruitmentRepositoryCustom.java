468d67d3a4237b6ae842cc7c1924a334f1863689
parksangwoo <b01056721617@gmail.com>
2022-04-14 23:11:56 +0900

    ⚡️ :: (#155) 정렬 조건 파라미터 추가

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepositoryCustom.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepositoryCustom.java
@@ -1,13 +1,15 @@
 package com.jobits.dsm.benecia.domain.recruitment.domain;
 
+import com.jobits.dsm.benecia.domain.recruitment.code.HiringAreaCode;
 import com.jobits.dsm.benecia.domain.recruitment.code.RecruitmentStatusCode;
 import com.jobits.dsm.benecia.domain.recruitment.domain.vo.CurrentRecruitmentInfoListForStudentVO;
 import com.jobits.dsm.benecia.domain.recruitment.domain.vo.RecruitmentInfoListForTeacherVO;
+import com.jobits.dsm.benecia.domain.recruitment.type.SortCondition;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public interface RecruitmentRepositoryCustom {
     List<RecruitmentInfoListForTeacherVO> getRecruitmentInfoList(String receptionYear, String keyword, RecruitmentStatusCode recruitStatus, LocalDate beginDate, LocalDate endDate);
-    List<CurrentRecruitmentInfoListForStudentVO> getCurrentRecruitmentInfoList();
+    List<CurrentRecruitmentInfoListForStudentVO> getCurrentRecruitmentInfoList(List<Integer> tags, List<HiringAreaCode> hirings, String keyword, Integer region, SortCondition sort);
 }
