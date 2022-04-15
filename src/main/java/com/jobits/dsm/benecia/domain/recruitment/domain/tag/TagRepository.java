3eb17bdf05fb5a3e0f8c379d5cd5b930bc2e376f
parksangwoo <b01056721617@gmail.com>
2022-04-15 09:06:17 +0900

    ⚡️ :: (#153) 검색 쿼리메소드 추가

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/tag/TagRepository.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/tag/TagRepository.java
@@ -2,5 +2,8 @@ package com.jobits.dsm.benecia.domain.recruitment.domain.tag;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface TagRepository extends CrudRepository<Tag, Integer> {
+    List<Tag> findByNameContains(String keyword);
 }
