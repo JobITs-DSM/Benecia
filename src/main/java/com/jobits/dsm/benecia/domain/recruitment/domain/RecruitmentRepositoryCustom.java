128239dca23ca18dca500c374cdcebd651a4c597
parksangwoo <b01056721617@gmail.com>
2022-04-16 23:44:25 +0900

    📑 :: (#156) RecruitmentDetailResponse DTO

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepositoryCustom.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/RecruitmentRepositoryCustom.java
@@ -3,6 +3,7 @@ package com.jobits.dsm.benecia.domain.recruitment.domain;
 import com.jobits.dsm.benecia.domain.recruitment.code.HiringAreaCode;
 import com.jobits.dsm.benecia.domain.recruitment.code.RecruitmentStatusCode;
 import com.jobits.dsm.benecia.domain.recruitment.domain.vo.CurrentRecruitmentInfoListForStudentVO;
+import com.jobits.dsm.benecia.domain.recruitment.domain.vo.RecruitmentDetailVO;
 import com.jobits.dsm.benecia.domain.recruitment.domain.vo.RecruitmentInfoListForTeacherVO;
 import com.jobits.dsm.benecia.domain.recruitment.type.SortCondition;
 
@@ -12,4 +13,5 @@ import java.util.List;
 public interface RecruitmentRepositoryCustom {
     List<RecruitmentInfoListForTeacherVO> getRecruitmentInfoList(String receptionYear, String keyword, RecruitmentStatusCode recruitStatus, LocalDate beginDate, LocalDate endDate);
     List<CurrentRecruitmentInfoListForStudentVO> getCurrentRecruitmentInfoList(List<Integer> tagIds, List<HiringAreaCode> hiringAreaCodes, String keyword, Integer regionId, SortCondition sort);
+    List<RecruitmentDetailVO> queryRecruitmentDetail(Integer hiringId);
 }
