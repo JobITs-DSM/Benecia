2857d428d457712c5a666807ebf73808f655b617
parksangwoo <b01056721617@gmail.com>
2022-04-17 16:33:29 +0900

    ⚡️ :: (#156) findAllByRecruitment 메소드 추가

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/welfare/WelfareRepository.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/welfare/WelfareRepository.java
@@ -1,6 +1,12 @@
 package com.jobits.dsm.benecia.domain.recruitment.domain.welfare;
 
+import com.jobits.dsm.benecia.domain.recruitment.code.WelfareCode;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface WelfareRepository extends JpaRepository<Welfare, Integer> {
+    @Query("SELECT w.code FROM Welfare w WHERE w.recruitment.recruitmentId.receptionYear = :receptionYear and w.recruitment.recruitmentId.registrationNumber = :registrationNumber")
+    List<WelfareCode> findAllByRecruitment(String receptionYear, String registrationNumber);
 }
