a4d96fe566672d19d01fff70446ab40ca10bbd26
parksangwoo <b01056721617@gmail.com>
2022-04-21 19:19:32 +0900

    📑 :: (#183) SimilarRecruitmentInfoListForStudentVO

---
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/domain/vo/SimilarRecruitmentInfoListForStudentVO.java
@@ -0,0 +1,31 @@
+package com.jobits.dsm.benecia.domain.recruitment.domain.vo;
+
+import com.jobits.dsm.benecia.domain.recruitment.code.HiringAreaCode;
+import com.querydsl.core.annotations.QueryProjection;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class SimilarRecruitmentInfoListForStudentVO {
+    private final Integer hiringId;
+    private final HiringAreaCode hiringCode;
+    private final Integer recruitCount;
+    private final String enterpriseName;
+    private final String workPlace;
+    private final List<String> tags;
+    private final String enterpriseProfileImageUrl;
+    private final String enterpriseBackgroundImageUrl;
+
+    @QueryProjection
+    public SimilarRecruitmentInfoListForStudentVO(Integer hiringId, HiringAreaCode hiringCode, Integer recruitCount, String enterpriseName, String workPlace, List<String> tags, String enterpriseProfileImageUrl, String enterpriseBackgroundImageUrl) {
+        this.hiringId = hiringId;
+        this.hiringCode = hiringCode;
+        this.recruitCount = recruitCount;
+        this.enterpriseName = enterpriseName;
+        this.workPlace = workPlace;
+        this.tags = tags;
+        this.enterpriseProfileImageUrl = enterpriseProfileImageUrl;
+        this.enterpriseBackgroundImageUrl = enterpriseBackgroundImageUrl;
+    }
+}
