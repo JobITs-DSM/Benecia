924abc6fe9e68cb097e7f867bcd8db23bb4334ed
parksangwoo <b01056721617@gmail.com>
2022-04-30 02:04:28 +0900

    ⚡️ :: (#189) findByEnterpriseAndDivision 메소드 추가

--- src/main/java/com/jobits/dsm/benecia/domain/review/domain/ReviewRepository.java
+++ src/main/java/com/jobits/dsm/benecia/domain/review/domain/ReviewRepository.java
@@ -1,6 +1,11 @@
 package com.jobits.dsm.benecia.domain.review.domain;
 
+import com.jobits.dsm.benecia.domain.enterprise.domain.Enterprise;
+import com.jobits.dsm.benecia.domain.review.code.ReviewCode;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
+    List<Review> findByEnterpriseAndDivision(Enterprise enterprise, ReviewCode division);
 }
