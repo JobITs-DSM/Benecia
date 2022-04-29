d29684878732ce84cfa9dbba5027488c22719b55
parksangwoo <b01056721617@gmail.com>
2022-04-30 02:06:01 +0900

    ⚡️ :: (#189) queryEnterpriseReviewForStudent 메소드 추가

--- src/main/java/com/jobits/dsm/benecia/domain/review/service/ReviewService.java
+++ src/main/java/com/jobits/dsm/benecia/domain/review/service/ReviewService.java
@@ -3,9 +3,11 @@ package com.jobits.dsm.benecia.domain.review.service;
 import com.jobits.dsm.benecia.domain.enterprise.domain.Enterprise;
 import com.jobits.dsm.benecia.domain.enterprise.domain.EnterpriseRepository;
 import com.jobits.dsm.benecia.domain.enterprise.exceptions.EnterpriseNotFoundException;
+import com.jobits.dsm.benecia.domain.review.code.ReviewCode;
 import com.jobits.dsm.benecia.domain.review.domain.Review;
 import com.jobits.dsm.benecia.domain.review.domain.ReviewRepository;
-import com.jobits.dsm.benecia.domain.review.presentation.payload.RegisterTrainingReviewRequest;
+import com.jobits.dsm.benecia.domain.review.presentation.payload.request.RegisterTrainingReviewRequest;
+import com.jobits.dsm.benecia.domain.review.presentation.payload.response.QueryEnterpriseReviewForStudent;
 import com.jobits.dsm.benecia.domain.student.domain.Student;
 import com.jobits.dsm.benecia.domain.student.domain.StudentRepository;
 import com.jobits.dsm.benecia.domain.student.exceptions.StudentNotFoundException;
@@ -13,8 +15,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -39,5 +41,31 @@ public class ReviewService {
                 .student(student)
                 .build();
         reviewRepository.save(review);
+    }
+
+    @Transactional(readOnly = true)
+    public QueryEnterpriseReviewForStudent queryEnterpriseReviewForStudent(String registrationNumber) {
+        Enterprise enterprise = enterpriseRepository.findById(registrationNumber)
+                        .orElseThrow(() -> EnterpriseNotFoundException.EXCEPTION);
+        List<QueryEnterpriseReviewForStudent.ReviewInfo> interviewReview = reviewRepository.findByEnterpriseAndDivision(enterprise, ReviewCode.INTERVIEW_REVIEW)
+                .stream().map(interviewReviews -> QueryEnterpriseReviewForStudent.ReviewInfo.builder()
+                        .userName(interviewReviews.getStudent().getName())
+                        .userProfileImageUrl(interviewReviews.getStudent().getProfileImage().getFileName())
+                        .content(interviewReviews.getContent())
+                        .build()
+                ).toList();
+
+        List<QueryEnterpriseReviewForStudent.ReviewInfo> trainingReview = reviewRepository.findByEnterpriseAndDivision(enterprise, ReviewCode.TRAINING_REVIEW)
+                .stream().map(trainingReviews -> QueryEnterpriseReviewForStudent.ReviewInfo.builder()
+                        .userName(trainingReviews.getStudent().getName())
+                        .userProfileImageUrl(trainingReviews.getStudent().getProfileImage().getFileName())
+                        .content(trainingReviews.getContent())
+                        .build()
+                ).toList();
+
+        return QueryEnterpriseReviewForStudent.builder()
+                .interviewReviewList(interviewReview)
+                .trainingReviewList(trainingReview)
+                .build();
     }
 }
