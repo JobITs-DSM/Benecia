5514f2927fb10618f2b889fcf76b7f22a3be6bf8
parksangwoo <b01056721617@gmail.com>
2022-04-30 21:22:46 +0900

    📑 :: (#195) 라우터 추가

---
+++ src/main/java/com/jobits/dsm/benecia/domain/application/presentation/ApplicationController.java
@@ -0,0 +1,22 @@
+package com.jobits.dsm.benecia.domain.application.presentation;
+
+import com.jobits.dsm.benecia.domain.application.presentation.payload.request.ApplyEnterpriseRequest;
+import com.jobits.dsm.benecia.domain.application.service.ApplicationService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("/application")
+@RequiredArgsConstructor
+public class ApplicationController {
+    private final ApplicationService applicationService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void ApplyEnterprise(@RequestBody @Valid ApplyEnterpriseRequest request) {
+        applicationService.applyEnterprise(request);
+    }
+}
