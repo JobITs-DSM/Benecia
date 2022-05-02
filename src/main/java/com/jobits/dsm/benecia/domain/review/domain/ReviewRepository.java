d750eb2095e25ffd5bf39d5e8ea2c5c9cb080fe2
parksangwoo <b01056721617@gmail.com>
2022-05-02 13:46:58 +0900

    ⚡️ :: (#189) 한 번에 다 조회해오도록 변경

--- src/main/java/com/jobits/dsm/benecia/domain/review/domain/ReviewRepository.java
+++ src/main/java/com/jobits/dsm/benecia/domain/review/domain/ReviewRepository.java
@@ -7,5 +7,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface ReviewRepository extends JpaRepository<Review, Integer> {
-    List<Review> findByEnterpriseAndDivision(Enterprise enterprise, ReviewCode division);
+    List<Review> findByEnterprise(Enterprise enterprise);
 }
