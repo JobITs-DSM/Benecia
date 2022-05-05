4b95bd8c51d9501b7dc2286bb40c98ce8db6fbdb
parksangwoo <b01056721617@gmail.com>
2022-05-05 17:55:16 +0900

    ⚡️ :: (#199) removeEnterpriseReview 메소드 추가

--- src/main/java/com/jobits/dsm/benecia/domain/review/presentation/ReviewController.java
+++ src/main/java/com/jobits/dsm/benecia/domain/review/presentation/ReviewController.java
@@ -4,6 +4,7 @@ import com.jobits.dsm.benecia.domain.review.presentation.payload.request.Registe
 import com.jobits.dsm.benecia.domain.review.presentation.payload.response.QueryEnterpriseReviewForStudent;
 import com.jobits.dsm.benecia.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,5 +24,11 @@ public class ReviewController {
     @GetMapping("/{registration-number}")
     public QueryEnterpriseReviewForStudent queryEnterpriseReviewForStudent(@PathVariable("registration-number") String registrationNumber) {
         return reviewService.queryEnterpriseReviewForStudent(registrationNumber);
+    }
+
+    @DeleteMapping("/{review-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void removeEnterpriseReview(@PathVariable("review-id") Integer reviewId) {
+        reviewService.removeEnterpriseReview(reviewId);
     }
 }
