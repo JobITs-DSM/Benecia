4327d49ea1d7d10aa21b93c826819a0dfd778c38
parksangwoo <b01056721617@gmail.com>
2022-05-05 20:25:37 +0900

    ⚡️ :: (#203) 라우터 추가

--- src/main/java/com/jobits/dsm/benecia/domain/review/presentation/ReviewController.java
+++ src/main/java/com/jobits/dsm/benecia/domain/review/presentation/ReviewController.java
@@ -4,6 +4,7 @@ import com.jobits.dsm.benecia.domain.review.presentation.payload.request.Registe
 import com.jobits.dsm.benecia.domain.review.presentation.payload.response.QueryEnterpriseReviewForStudent;
 import com.jobits.dsm.benecia.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,5 +24,11 @@ public class ReviewController {
     @GetMapping("/{registration-number}")
     public QueryEnterpriseReviewForStudent queryEnterpriseReviewForStudent(@PathVariable("registration-number") String registrationNumber) {
         return reviewService.queryEnterpriseReviewForStudent(registrationNumber);
+    }
+
+    @PatchMapping("/admin/{review-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void checkEnterpriseReviewForTeacher(@PathVariable("review-id") Integer reviewId) {
+        reviewService.checkEnterpriseReviewForTeacher(reviewId);
     }
 }
