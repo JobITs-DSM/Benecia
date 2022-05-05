a0fb81768e3dba145bd82caf664cbe028f7fb51b
parksangwoo <b01056721617@gmail.com>
2022-05-05 18:41:11 +0900

    ⚡️ :: (#201) 라우터 추가

--- src/main/java/com/jobits/dsm/benecia/domain/review/presentation/ReviewController.java
+++ src/main/java/com/jobits/dsm/benecia/domain/review/presentation/ReviewController.java
@@ -1,9 +1,11 @@
 package com.jobits.dsm.benecia.domain.review.presentation;
 
 import com.jobits.dsm.benecia.domain.review.presentation.payload.request.RegisterTrainingReviewRequest;
+import com.jobits.dsm.benecia.domain.review.presentation.payload.request.ReviseEnterpriseReviewRequest;
 import com.jobits.dsm.benecia.domain.review.presentation.payload.response.QueryEnterpriseReviewForStudent;
 import com.jobits.dsm.benecia.domain.review.service.ReviewService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -23,5 +25,12 @@ public class ReviewController {
     @GetMapping("/{registration-number}")
     public QueryEnterpriseReviewForStudent queryEnterpriseReviewForStudent(@PathVariable("registration-number") String registrationNumber) {
         return reviewService.queryEnterpriseReviewForStudent(registrationNumber);
+    }
+
+    @PatchMapping("/{review-id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void reviseEnterpriseReview(@PathVariable("review-id") Integer reviewId,
+                                       @RequestBody @Valid ReviseEnterpriseReviewRequest request) {
+        reviewService.reviseEnterpriseReview(request, reviewId);
     }
 }
