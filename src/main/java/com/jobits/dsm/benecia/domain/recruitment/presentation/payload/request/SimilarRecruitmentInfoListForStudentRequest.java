4d277d54aaff7b149cbec737c2fbc3e2401116ef
parksangwoo <b01056721617@gmail.com>
2022-05-10 17:07:30 +0900

    ⚡️ :: (#211) NoArgs -> AllArgs로 변경

--- src/main/java/com/jobits/dsm/benecia/domain/recruitment/presentation/payload/request/SimilarRecruitmentInfoListForStudentRequest.java
+++ src/main/java/com/jobits/dsm/benecia/domain/recruitment/presentation/payload/request/SimilarRecruitmentInfoListForStudentRequest.java
@@ -1,14 +1,14 @@
 package com.jobits.dsm.benecia.domain.recruitment.presentation.payload.request;
 
 import com.jobits.dsm.benecia.domain.recruitment.code.HiringAreaCode;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
-@NoArgsConstructor
+@AllArgsConstructor
 public class SimilarRecruitmentInfoListForStudentRequest {
     @NotNull
     private List<HiringAreaCode> hiringCodes;
