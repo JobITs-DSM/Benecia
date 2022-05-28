84fe5ff009a3138c9ad4428fede5a9af33fd9dfe
parksangwoo <b01056721617@gmail.com>
2022-05-28 14:05:23 +0900

    ⚡️ :: (#224) extends 추가

--- src/main/java/com/jobits/dsm/benecia/domain/application/domain/ApplicationRepository.java
+++ src/main/java/com/jobits/dsm/benecia/domain/application/domain/ApplicationRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
-public interface ApplicationRepository extends JpaRepository<Application, Integer>{
+public interface ApplicationRepository extends JpaRepository<Application, Integer>, ApplicationRepositoryCustom {
     @Query("SELECT a FROM Application a WHERE a.recruitment.recruitmentId.registrationNumber = :registrationNumber AND a.recruitment.recruitmentId.receptionYear = :receptionYear")
     List<Application> findAllByRecruitment(String registrationNumber, String receptionYear);
 }
