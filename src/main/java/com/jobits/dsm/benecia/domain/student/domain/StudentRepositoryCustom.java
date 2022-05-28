9a18d084c922ed7422aaa622a39403f524cf0cf6
parksangwoo <b01056721617@gmail.com>
2022-05-28 15:29:06 +0900

    ⚡️ :: (#231) searchStudentList 메소드 추가

--- src/main/java/com/jobits/dsm/benecia/domain/student/domain/StudentRepositoryCustom.java
+++ src/main/java/com/jobits/dsm/benecia/domain/student/domain/StudentRepositoryCustom.java
@@ -1,7 +1,11 @@
 package com.jobits.dsm.benecia.domain.student.domain;
 
+import com.jobits.dsm.benecia.domain.student.domain.vo.SearchStudentListVO;
 import com.jobits.dsm.benecia.domain.student.domain.vo.StudentCurrentStatusVO;
+
+import java.util.List;
 
 public interface StudentRepositoryCustom {
     StudentCurrentStatusVO queryStudentCurrentStatus(String serialNumber);
+    List<SearchStudentListVO> searchStudentList(String name);
 }
